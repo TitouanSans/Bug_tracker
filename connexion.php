9d802bd05d71912e7f4a68e05d7f718830e770fe
TitouanSans <Titouan.sanshochart@gmail.com>
2021-03-05 12:03:46 +0100

    first commit

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+
+#			Type de BDD    serveur     nom de la BDD,  user , Pass
+$db = new PDO('mysql:host=localhost;dbname=bug_tracker', 'root', 'root');
+# Réglage de l'affichage des erreurs
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+# Réglage de l'encodage de caractères
+$db->exec("SET CHARACTER SET utf8");
