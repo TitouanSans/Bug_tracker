ed0d4f336fc1cd5999ce743148d6896d3a9caa08
TitouanSans <Titouan.sanshochart@gmail.com>
2021-03-10 14:07:57 +0100

    bug form html

--- index.tpl.php
+++ index.tpl.php
@@ -7,6 +7,19 @@
 	<title>Bug tracker</title>
 </head>
 <body>
-
+<form method="post" action="action.php">
+	<p>Titre : <input class="form-control" type="text" name="titre"></p>
+	<p>Date : <input class="form-control" type="text" name="date"></p>
+	<p>
+		Titre : <select name="type" id="">
+			<option value="404">Erreur 404</option>
+			<option value="affichage">Bug d'affichage</option>
+			<option value="securite">Bug de sécurité</option>
+			<option value="bdd">Bug de base de donnée</option>
+		</select>
+	</p>
+	<p>Description : <input class="form-control" type="text" name="description"></p>
+	<button type="submit">Enregistrer</button>
+</form>
 </body>
 </html>
