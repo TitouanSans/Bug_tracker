2d394622b0e21e6490887067f56927529620dece
TitouanSans <Titouan.sanshochart@gmail.com>
2021-03-24 10:07:43 +0100

    test gitignore

--- connexion.php
+++ connexion.php
@@ -1,8 +1,9 @@
 <?php
-
+#test
 # Type de BDD    serveur     nom de la BDD,  user , Pass
 $db = new PDO('mysql:host=localhost;dbname=bug_tracker', 'root', 'root');
 # Réglage de l'affichage des erreurs
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 # Réglage de l'encodage de caractères
 $db->exec("SET CHARACTER SET utf8");
+ 
